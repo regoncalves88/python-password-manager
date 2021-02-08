6b93fa60950387f0dff93f8fe6e0596f512aa9e5
Renato Gonçalves <renatogoncalves@Renatos-MacBook-Pro.local>
2021-02-08 18:49:27 -0300

    Create buttons, labels and entries on canvas.

--- main.py
+++ main.py
@@ -5,14 +5,46 @@ from tkinter import *
 # --- SAVE PASSWORD --- #
 
 # --- UI SETUP --- #
+# Window
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
+
+# Canvas
+logo_img = PhotoImage(file="logo.png")
 
 canvas = Canvas(width=200, height=200)
-
-logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+# Buttons
+add_button = Button(text="Add", width=37)
+add_button.grid(column=1, row=4, columnspan=2)
+
+generate_button = Button(text="Generate Password")
+generate_button.grid(column=2, row=3)
+
+# Entries
+password_entry = Entry(width=22, justify="center", fg="gray")
+password_entry.insert(END, "* Required")
+password_entry.grid(column=1, row=3)
+
+username_entry = Entry(width=36, justify="center", fg="gray")
+username_entry.insert(END, "* Required")
+username_entry.grid(column=1, row=2, columnspan=2)
+
+website_entry = Entry(width=36, justify="center", fg="gray")
+website_entry.insert(END, "* Required")
+website_entry.grid(column=1, row=1, columnspan=2)
+
+# Labels
+password_label = Label(text="Password")
+password_label.grid(column=0, row=3)
+
+username_label = Label(text="E-mail / Username:")
+username_label.grid(column=0, row=2)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
 
 canvas.mainloop()
